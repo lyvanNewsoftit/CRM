066ba86a529af5d463662231627f95af42cda0bd
lyvanNewsoftit <lyvan.galonde@newsoftit.com>
2025-01-07 09:11:00 +0100

    Initialisation de l'API CRM: CRéation de l'entité USERS, et début de la mise en place de l'OAuth 2.0 avec google et azure.

---
+++ src/Kernel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+
+
+}
+
