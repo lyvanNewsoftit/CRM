3dc3939a0f10934381ff95a998a839820c7b5dc3
lyvanNewsoftit <lyvan.galonde@newsoftit.com>
2025-01-08 11:58:48 +0100

    installation de NelmioCorsBundle ain de géréer les paramètre CORS lors des requêtes API

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
